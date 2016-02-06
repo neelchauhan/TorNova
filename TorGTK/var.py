dec733b9c5f0fc849c8fcc2b75d98da8076fd463
Neel Chauhan <neel@neelc.org>
2016-02-06 11:20:39 -0500

    Added code for storing application logfiles (temporarily, while TorGTK is open)

--- TorGTK/var.py
+++ TorGTK/var.py
@@ -1,4 +1,6 @@
 from gi.repository import Gtk
+import tempfile
+import os.path
 
 # Define default port numbers
 default_socks_port = 19050
@@ -6,6 +8,10 @@ default_control_port = 19051
 
 # Tor process descriptor placeholder
 tor_process = None
+
+# Tor logfile location placeholder
+tor_logfile_dir = tempfile.mkdtemp()
+tor_logfile_location = os.path.join(tor_logfile_dir, "tor_log")
 
 # Define object dictionary
 objs = { }
