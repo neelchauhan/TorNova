15b348b98f3bb409aeca6d6d0d6025bcb8806f8e
Neel Chauhan <neel@neelc.org>
2016-02-12 19:15:20 -0500

    Describe design of list_elements.py in it's comments

--- TorGTK/list_elements.py
+++ TorGTK/list_elements.py
@@ -3,12 +3,19 @@ from ui_elements import *
 from gi.repository import Gtk
 from torctl import *
 
+# ORGANIZATION OF THESE LISTS:
+# 1. Main list for all the elements
+# 2. A sublist for each element, with the first being a label, and the second
+#    being the element itself
+
+# List for main listbox
 lb_main_elements = [
 	["Enable Tor", init_switch("swEnable", enableTor)],
 	["", init_button("btnLog", "Log", show_log_win)],
 	["", init_button("btnAbout", "About", about_box)],
 ]
 
+# List for settings listbox
 lb_settings_elements = [
 	["SOCKS Port", init_spinbutton("spinSocks", default_socks_port, 1024, 65535, 1)],
 	["Control Port", init_spinbutton("spinCtl", default_control_port, 1024, 65535, 1)],
